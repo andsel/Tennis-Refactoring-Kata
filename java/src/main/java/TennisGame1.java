187dbaac3392f36ab4c2b0a66e633cb36e22352b
andsel <selva.andre@gmail.com>
2015-12-28 23:42:49 +0100

    Refactored game 1

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -12,65 +12,49 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        //== checks the instance id
+        if (playerName.equals("player1"))
+            m_score1++;
         else
-            m_score2 += 1;
+            m_score2++;
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
+        String score;
         if (m_score1==m_score2)
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+            if (m_score1 <= 2)
+                score = scoreToLabel(m_score1) + "-All";
+            else
+                score = "Deuce";
         }
         else if (m_score1>=4 || m_score2>=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            int scoreDistance = m_score1-m_score2;
+            final String prefix = Math.abs(scoreDistance) == 1 ? "Advantage " : "Win for ";
+            final String playerName = m_score1 > m_score2 ? player1Name : player2Name;
+            score = prefix + playerName;
         }
         else
         {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            score = scoreToLabel(m_score1) + "-" + scoreToLabel(m_score2);
         }
         return score;
+    }
+
+    private String scoreToLabel(int score) {
+        switch(score)
+        {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                return "";
+        }
     }
 }

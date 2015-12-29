6492851dd513a643526d5699a9d3fab72b8e979f
andsel <selva.andre@gmail.com>
2015-12-29 18:49:12 +0100

    Simplify the case of parity

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -16,19 +16,14 @@ public class TennisGame2 implements TennisGame
 
     public String getScore(){
         String score = "";
-        if (P1point == P2point && P1point < 4)
+        if (P1point == P2point)
         {
-            if (P1point==0)
-                score = "Love";
-            if (P1point==1)
-                score = "Fifteen";
-            if (P1point==2)
-                score = "Thirty";
-            score += "-All";
+            if (P1point>=3)
+                score = "Deuce";
+            else
+                score = scoreToLabel(P1point) + "-All";
         }
-        if (P1point==P2point && P1point>=3)
-            score = "Deuce";
-        
+
         if (P1point > 0 && P2point==0)
         {
             if (P1point==1)
@@ -37,7 +32,7 @@ public class TennisGame2 implements TennisGame
                 P1res = "Thirty";
             if (P1point==3)
                 P1res = "Forty";
-            
+
             P2res = "Love";
             score = P1res + "-" + P2res;
         }
@@ -49,11 +44,11 @@ public class TennisGame2 implements TennisGame
                 P2res = "Thirty";
             if (P2point==3)
                 P2res = "Forty";
-            
+
             P1res = "Love";
             score = P1res + "-" + P2res;
         }
-        
+
         if (P1point>P2point && P1point < 4)
         {
             if (P1point==2)
@@ -81,55 +76,47 @@ public class TennisGame2 implements TennisGame
         
         if (P1point > P2point && P2point >= 3)
         {
-            score = "Advantage player1";
+            score = "Advantage " + player1Name;
         }
         
         if (P2point > P1point && P1point >= 3)
         {
-            score = "Advantage player2";
+            score = "Advantage " + player2Name;
         }
         
         if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
         {
-            score = "Win for player1";
+            score = "Win for " + player1Name;
         }
         if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
         {
-            score = "Win for player2";
+            score = "Win for " + player2Name;
         }
         return score;
     }
     
-    public void SetP1Score(int number){
-        
-        for (int i = 0; i < number; i++)
-        {
-            P1Score();
-        }
-            
-    }
-    
-    public void SetP2Score(int number){
-        
-        for (int i = 0; i < number; i++)
-        {
-            P2Score();
-        }
-            
-    }
-    
-    public void P1Score(){
-        P1point++;
-    }
-    
-    public void P2Score(){
-        P2point++;
-    }
+    //NB removed unused code
 
     public void wonPoint(String player) {
-        if (player == "player1")
-            P1Score();
+        if (player.equals("player1"))
+            P1point++;
         else
-            P2Score();
+            P2point++;
+    }
+
+    private String scoreToLabel(int score) {
+        switch(score)
+        {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                return "";
+        }
     }
 }

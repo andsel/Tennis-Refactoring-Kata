ca799820dc2515b7aab62d26af2f7040ecca9ccc
andsel <selva.andre@gmail.com>
2015-12-29 19:15:26 +0100

    Removed invariants

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -36,22 +36,22 @@ public class TennisGame2 implements TennisGame
             P2res = scoreToLabel(P2point);
             score = P1res + "-" + P2res;
         }
-        
+
         if (P1point > P2point && P2point >= 3)
         {
             score = "Advantage " + player1Name;
         }
-        
+
         if (P2point > P1point && P1point >= 3)
         {
             score = "Advantage " + player2Name;
         }
-        
-        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
+
+        if (P1point>=4 && (P1point-P2point)>=2)
         {
             score = "Win for " + player1Name;
         }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
+        if (P2point>=4 && (P2point-P1point)>=2)
         {
             score = "Win for " + player2Name;
         }

a3dc96ef82575f95b30abe8efdb2decc3be530e8
andsel <selva.andre@gmail.com>
2016-01-29 10:02:56 +0100

    Least but no last

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -47,27 +47,30 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName.equals("player1"))
+        if (playerName.equals("player1")) {
             player1.wonPoint();
-        else
+        } else {
             player2.wonPoint();
+        }
     }
 
     public String getScore() {
-        if (player1.score() == player2.score())
-        {
-            if (player1.score() <= 2)
+        if (player1.score() == player2.score()) {
+            if (player1.score() <= 2) {
                 return player1.scoreToLabel() + "-All";
-            else
-                return "Deuce";
+            }
+            return "Deuce";
         }
-        if (player1.score()>=4 || player2.score()>=4)
-        {
-            int scoreDistance = player1.score()-player2.score();
-            final String prefix = Math.abs(scoreDistance) == 1 ? "Advantage " : "Win for ";
+        if (player1.score() >= 4 || player2.score() >= 4) {
+            final String prefix = advantageType(player1, player2);
             final Player advantaged = player1.vantage(player2);
             return prefix + advantaged.name;
         }
         return player1.scoreToLabel() + "-" + player2.scoreToLabel();
+    }
+
+    String advantageType(Player player1, Player player2) {
+        int scoreDistance = player1.score() - player2.score();
+        return Math.abs(scoreDistance) == 1 ? "Advantage " : "Win for ";
     }
 }

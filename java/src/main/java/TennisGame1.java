dd996398d597f5361cc8498dbb370f5fa87376ca
andsel <selva.andre@gmail.com>
2016-07-23 17:31:43 +0200

    Normalized fields names

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,8 +1,8 @@
 
 public class TennisGame1 implements TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+
+    private int score1 = 0;
+    private int score2 = 0;
     private String player1Name;
     private String player2Name;
 
@@ -13,16 +13,16 @@ public class TennisGame1 implements TennisGame {
 
     public void wonPoint(String playerName) {
         if (playerName.equals("player1"))
-            m_score1 += 1;
+            score1 += 1;
         else
-            m_score2 += 1;
+            score2 += 1;
     }
 
     public String getScore() {
         String score = "";
-        if (m_score1==m_score2)
+        if (score1 == score2)
         {
-            switch (m_score1)
+            switch (score1)
             {
                 case 0:
                         score = "Love-All";
@@ -39,9 +39,9 @@ public class TennisGame1 implements TennisGame {
                 
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (score1 >=4 || score2 >=4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = score1 - score2;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -52,8 +52,8 @@ public class TennisGame1 implements TennisGame {
             int tempScore=0;
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = score1;
+                else { score+="-"; tempScore = score2;}
                 switch(tempScore)
                 {
                     case 0:

db490e253f419c8bf5434c4417ba4d65e7febcf8
andsel <selva.andre@gmail.com>
2016-07-23 17:50:10 +0200

    Introduced use of player's name

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -52,7 +52,7 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName.equals("player1"))
+        if (playerName.equals(player1Name))
             score1.wonPoint();
         else
             score2.wonPoint();
@@ -71,10 +71,10 @@ public class TennisGame1 implements TennisGame {
         else if (score1.value >=4 || score2.value >=4)
         {
             int minusResult = score1.value - score2.value;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            if (minusResult==1) score ="Advantage " + player1Name;
+            else if (minusResult ==-1) score ="Advantage " + player2Name;
+            else if (minusResult>=2) score = "Win for " + player1Name;
+            else score ="Win for " + player2Name;
         }
         else
         {
